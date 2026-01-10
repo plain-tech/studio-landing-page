df94dab49c08b8d7a29566d3372d269d8f55ff12
BMalaichik <boris.malaichik@gmail.com>
2026-01-10 11:51:59 -0800

    update socials

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -1,25 +1,23 @@
 import { IMenuItem, ISocials } from "@/types";
 
 export const footerDetails: {
-    subheading: string;
-    quickLinks: IMenuItem[];
-    email: string;
-    telephone: string;
-    socials: ISocials;
+  subheading: string;
+  quickLinks: IMenuItem[];
+  email: string;
+  telephone: string;
+  socials: ISocials;
 } = {
   subheading:
     "AI-powered interior design visualisation. Generate & edit client-ready renders in seconds.",
-    quickLinks: [
+  quickLinks: [
     { text: "Features", url: "#features" },
     { text: "Pricing", url: "#pricing" },
     { text: "Testimonials", url: "#testimonials" },
     { text: "FAQ", url: "#faq" },
-    ],
+  ],
   email: "hello@plain-service.com",
   telephone: "",
-    socials: {
-    twitter: "https://twitter.com/",
-    instagram: "https://www.instagram.com/",
-    linkedin: "https://www.linkedin.com/",
+  socials: {
+    instagram: "https://www.instagram.com/therenderai/",
   },
 };
